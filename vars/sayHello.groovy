68a160ec92b922e646cf6cc903c698d848e34396
Rahul Suthar <RahulSuthar@users.noreply.github.com>
2020-01-13 18:05:47 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  echo "Hello, ${name}."
+  echo "--------------SharedLibraryMethodCall--------------"
+  echo "Hello , ${name}!! SharedLibrary method call successful!!"
+  echo "-------------------------End-----------------------"
 }
